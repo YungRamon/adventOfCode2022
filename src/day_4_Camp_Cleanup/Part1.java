0e243b0150aab658e9b348c146c7ffb82f2ba566
Anton Kartashov <tohakartasov@gmail.com>
2022-12-28 21:57:26 +0100

    Day 4

---
+++ src/day_4_Camp_Cleanup/Part1.java
@@ -0,0 +1,28 @@
+package day_4_Camp_Cleanup;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Part1 {
+	
+	
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		// Open file with contents
+		//File file=new File("D:/fuentes_advent/4_ex.txt");
+		File file=new File("D:/fuentes_advent/4.txt");
+		Scanner reader=new Scanner(file);
+		int cont=0;
+		while (reader.hasNext()) {
+			//Get the sectors
+			 String[] sectores = reader.nextLine().split("(-|,)");
+			 int[] nums=new int[sectores.length];
+			 for (int i = 0; i < nums.length; i++) {
+				nums[i]=Integer.valueOf(sectores[i]);
+			}
+			if((nums[0]>=nums[2] && nums[1]<=nums[3]) ||(nums[2]>=nums[0] && nums[3]<=nums[1])) cont++;
+		}
+		System.out.println(cont);
+	}
+}
